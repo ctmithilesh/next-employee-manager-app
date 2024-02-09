34b062ddd29c93c97426183215a5ef27e5eb526b
mithilesh <mithilesht@cloudthat.com>
2024-02-09 22:45:20 +0530

    minor bug fixes

--- src/app/add-employee/page.jsx
+++ src/app/add-employee/page.jsx
@@ -47,35 +47,32 @@ const Page = () => {
             <div className="hero bg-base-200">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className="text-center lg:text-left">
-                        <h1 className="text-5xl font-bold">Login now!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <h1 className="text-5xl font-bold">Add Employee!</h1>
                     </div>
                     <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                         <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Email</span>
+                                    <span className="label-text">First Name</span>
                                 </label>
                                 <input
                                     type="text"
-                                    placeholder="emp_first_name"
+                                    placeholder="First Name"
                                     className="input input-bordered"
                                     {...register("emp_first_name", { required: true })}
                                 />
                             </div>
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Password</span>
+                                    <span className="label-text">Address</span>
                                 </label>
                                 <input
                                     type="text"
-                                    placeholder="emp_address"
+                                    placeholder="Address"
                                     className="input input-bordered"
                                     {...register("emp_address", { required: true })}
                                 />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                </label>
+
                             </div>
                             <div className="form-control mt-6">
                                 <button className="btn btn-primary">Submit</button>
